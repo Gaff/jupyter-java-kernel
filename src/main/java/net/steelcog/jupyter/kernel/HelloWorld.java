45e6aa6761c50ef355e7bdc85901e36ebcec184f
Matthew Shaylor <github@shaylor.co.uk>
2016-05-22 00:19:13 +0100

    Initial commit

---
+++ src/main/java/net/steelcog/jupyter/kernel/HelloWorld.java
@@ -0,0 +1,10 @@
+package net.steelcog.jupyter.kernel;
+
+/**
+ * Created by Matthew on 22-May-16.
+ */
+public class HelloWorld {
+    static void main(String argv[]) {
+        System.out.println("Hello world\n");
+    }
+}
